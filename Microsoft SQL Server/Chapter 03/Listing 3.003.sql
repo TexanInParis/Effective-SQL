2c5d443e58e349c0762e5bfa972d3caa24040ac3
DJSteele <djsteele@gmail.com>
2016-04-16 17:59:33 -0400

    Cleanup of submitted items

---
+++ Microsoft SQL Server/Chapter 03/Listing 3.003.sql
@@ -0,0 +1,24 @@
+--Listing 3.3 Three view definitions
+
+CREATE VIEW vActiveCustomers AS
+SELECT c.CustomerID, c.CustFirstName, c.CustLastName, 
+  c.CustFirstName + ' ' + c.CustLastName AS CustFullName
+FROM Customers AS c
+WHERE EXISTS 
+ (SELECT NULL
+  FROM Orders AS o
+  WHERE o.CustomerID = c.CustomerID
+    AND o.OrderDate > DATEADD(MONTH, -6, GETDATE()));
+
+CREATE VIEW vCustomerStatistics AS 
+SELECT o.CustomerID, COUNT(o.OrderNumber) AS OrderCount,
+  SUM(o.OrderTotal) AS GrandOrderTotal, MAX(o.OrderDate) AS LastOrderDate
+FROM Orders AS o
+GROUP BY o.CustomerID;
+
+CREATE VIEW vActiveCustomerStatistics AS 
+SELECT a.CustomerID, a.CustFirstName, a.CustLastName, 
+  s.LastOrderDate, s.GrandOrderTotal
+FROM vActiveCustomers AS a
+INNER JOIN vCustomerStatistics AS s
+  ON a.CustomerID = s.CustomerID;
---
+++ Microsoft SQL Server/Chapter 03/Listing 3.004.sql
@@ -0,0 +1,16 @@
+--Listing 3.4 Equivalent statement of combined views
+
+SELECT c.CustomerID, c.CustFirstName, c.CustLastName, 
+  s.LastOrderDate, s.GrandOrderTotal
+FROM Customers AS c
+INNER JOIN 
+  (SELECT o.CustomerID, SUM(o.OrderTotal) AS GrandOrderTotal,
+    MAX(o.OrderDate) AS LastOrderDate  
+   FROM Orders AS o
+   GROUP BY o.CustomerID) AS s
+  ON c.CustomerID = s.CustomerID
+WHERE EXISTS 
+ (SELECT NULL
+  FROM Orders AS o
+  WHERE o.CustomerID = c.CustomerID
+    AND o.OrderDate > DATEADD(MONTH, -6, GETDATE())); 
