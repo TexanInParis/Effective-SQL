1365384b0ce31a94671284a325018059f7efa1b0
DJSteele <djsteele@gmail.com>
2016-04-30 08:57:21 -0400

    Changes to two listings
    Ben discovered SET STATISTICS PROFILE is being deprecated, so we changed
    it to SET STATISTICS XML

--- Microsoft SQL Server/Chapter 07/Listing 7.005.sql
+++ Microsoft SQL Server/Chapter 07/Listing 7.005.sql
@@ -1,3 +1,7 @@
 -- Listing 7.5 Enabling execution profiling in SQL Server
 
-SET STATISTICS PROFILE ON;
+-- Displays results in a tabular form, but about to be deprecated. 
+-- SET STATISTICS PROFILE ON;
+
+-- Use this instead to get the results in XML.
+SET STATISTICS XML ON;
--- Microsoft SQL Server/Chapter 07/Listing 7.006.sql
+++ Microsoft SQL Server/Chapter 07/Listing 7.006.sql
@@ -1,3 +1,8 @@
 -- Listing 7.6 Disabling execution profiling in SQL Server
 
-SET STATISTICS PROFILE OFF;
+-- Use this if you used SET STATISTICS PROFILE ON in Listing 7.5. 
+-- SET STATISTICS PROFILE OFF;
+
+-- Use this instead to get the results in XML.
+-- Use this if you used SET STATISTICS XML ON in Listing 7.5. 
+SET STATISTICS XML OFF;
