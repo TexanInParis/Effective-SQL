3e994a8bca2f7ed5c6e66f5ef01f5efe4ccde205
DJSteele <djsteele@gmail.com>
2016-05-18 16:52:50 -0400

    Re-addition of Doug's Chapter 8 listings.

---
+++ Microsoft SQL Server/Chapter 08/Listing 8.001.sql
@@ -0,0 +1,12 @@
+-- Ensure you've run SalesOrdersStructure.sql and SalesOrdersData.sql in order to run this example. 
+
+USE SalesOrdersSample;
+GO
+
+-- Listing 8.1 Using a Cartesian Product to obtain a list of every customer and every product
+
+SELECT C.CustomerID, C.CustFirstName, C.CustLastName,
+  P.ProductNumber, P.ProductName, P.ProductDescription
+FROM Customers AS C, Products AS P;
+
+
