7f359e473f95d1e4f4e3b84278f19dd7530aa258
TexanInParis <john@viescas.com>
2016-05-18 18:41:18 +0200

    Sample files

---
+++ Microsoft SQL Server/Chapter 03/Listing 3.005.sql
@@ -0,0 +1,35 @@
+SELECT CAST('2015-01-01' AS Date) AS SalesMonth, Jan AS SalesAmt
+  FROM tblPostSales
+UNION ALL
+SELECT CAST('2015-02-01' AS Date) AS SalesMonth, Feb AS SalesAmt
+  FROM tblPostSales
+UNION ALL
+SELECT CAST('2015-03-01' AS Date) AS SalesMonth, Mar AS SalesAmt
+  FROM tblPostSales
+UNION ALL
+SELECT CAST('2015-04-01' AS Date) AS SalesMonth, Apr AS SalesAmt
+  FROM tblPostSales
+UNION ALL
+SELECT CAST('2015-05-01' AS Date) AS SalesMonth, May AS SalesAmt
+  FROM tblPostSales
+UNION ALL
+SELECT CAST('2015-06-01' AS Date) AS SalesMonth, Jun AS SalesAmt
+  FROM tblPostSales
+UNION ALL
+SELECT CAST('2015-07-01' AS Date) AS SalesMonth, Jul AS SalesAmt
+  FROM tblPostSales
+UNION ALL
+SELECT CAST('2015-08-01' AS Date) AS SalesMonth, Aug AS SalesAmt
+  FROM tblPostSales
+UNION ALL
+SELECT CAST('2015-09-01' AS Date) AS SalesMonth, Sep AS SalesAmt
+  FROM tblPostSales
+UNION ALL
+SELECT CAST('2015-10-01' AS Date) AS SalesMonth, Oct AS SalesAmt
+  FROM tblPostSales
+UNION ALL
+SELECT CAST('2015-11-01' AS Date) AS SalesMonth, Nov AS SalesAmt
+  FROM tblPostSales
+UNION ALL
+SELECT CAST('2015-12-01' AS Date) AS SalesMonth, Dec AS SalesAmt
+  FROM tblPostSales;
