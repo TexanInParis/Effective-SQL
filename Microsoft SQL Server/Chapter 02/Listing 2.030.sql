86c4093914f9e044abec82a6f4272669a804da92
DJSteele <djsteele@gmail.com>
2016-05-19 15:48:09 -0400

    Sample file

--- Microsoft SQL Server/Chapter 02/Listing 2.030.sql
+++ Microsoft SQL Server/Chapter 02/Listing 2.030.sql
@@ -1,17 +1,19 @@
-ALTER TABLE Employees
-ADD EmpDOB date;
+-- Ensure you've run SalesOrdersStructure.sql and SalesOrdersData.sql in order to run this example. 
+
+USE SalesOrdersSample;
+GO
+
+-- Listing 2.30 SQL statement using CalculateAge function
 
 CREATE FUNCTION CalculateAge(@Date_of_Birth DATE) 
 RETURNS int AS
 BEGIN
   RETURN DATEDIFF(DAY, @Date_of_Birth, CAST(GETDATE() AS date)) / 365;
 END;
+GO
 
 SELECT EmployeeID, EmpFirstName, EmpLastName, dbo.CalculateAge(EmpDOB) AS EmpAge
   FROM Employees
- WHERE dbo.CalculateAge(EmpDOB) > 50;
+WHERE dbo.CalculateAge(EmpDOB) > 50;
 
 DROP FUNCTION CalculateAge;
-
-ALTER TABLE Employees
-DROP COLUMN EmpDOB;
