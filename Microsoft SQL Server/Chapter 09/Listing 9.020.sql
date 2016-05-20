b26d17426569c3a42b43897bd588b8bd3fb8b8ec
DJSteele <djsteele@gmail.com>
2016-05-20 14:36:44 -0400

    Sample Files

--- Microsoft SQL Server/Chapter 09/Listing 9.020.sql
+++ Microsoft SQL Server/Chapter 09/Listing 9.020.sql
@@ -1,10 +1,12 @@
--- Listing 9.20 Table creation DDL for an Appointment table
+-- Create a new database, to make it easier to cleanup afterwards.
 
 CREATE DATABASE Item56Example;
 GO
+
 USE Item56Example;
 GO
 
+-- Listing 9.20 Table creation DDL for an Appointment table
 CREATE TABLE Appointments (
   AppointmentID int IDENTITY (1, 1) PRIMARY KEY ,
   ApptStartDate date NOT NULL ,
@@ -13,10 +15,11 @@ CREATE TABLE Appointments (
   ApptEndTime time NOT NULL ,
   ApptDescription nvarchar (50) NULL 
 );
-
+GO
 
 INSERT INTO Appointments (ApptStartDate, ApptStartTime, ApptEndDate, ApptEndTime, ApptDescription) VALUES ('2017-01-03', '10:30', '2017-01-03', '11:00', 'Meet with John');
 INSERT INTO Appointments (ApptStartDate, ApptStartTime, ApptEndDate, ApptEndTime, ApptDescription) VALUES ('2017-01-03', '11:15', '2017-01-03', '12:00', 'Design cover page');
 INSERT INTO Appointments (ApptStartDate, ApptStartTime, ApptEndDate, ApptEndTime, ApptDescription) VALUES ('2017-01-05', '09:00', '2017-01-05', '15:00', 'Teach SQL course');
 INSERT INTO Appointments (ApptStartDate, ApptStartTime, ApptEndDate, ApptEndTime, ApptDescription) VALUES ('2017-01-05', '15:30', '2017-01-05', '16:30', 'Review with Ben');
 INSERT INTO Appointments (ApptStartDate, ApptStartTime, ApptEndDate, ApptEndTime, ApptDescription) VALUES ('2017-01-06', '10:00', '2017-01-06', '11:30', 'Plan for lunch');
+GO
