957b911e72e5636858c764af894172c0ba8eb2b4
TexanInParis <john@viescas.com>
2016-05-20 13:54:53 +0200

    Sample

--- Microsoft SQL Server/Chapter 01/Listing 1.007.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE Orders
-  OrderNumber int NOT NULL ,
-  OrderDate date NULL ,
-  ShipDate date NULL ,
-  CustomerID int NULL ,
-  EmployeeID int NULL ,
-  OrderTotal money AS dbo.getOrderTotal(OrderNumber));
