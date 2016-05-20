9b98431f01c837a08406fccaa6771ffcba1012cd
DJSteele <djsteele@gmail.com>
2016-05-20 14:17:24 -0400

    Sample Files

--- Microsoft SQL Server/Chapter 05/Listing 5.004.sql
+++ Microsoft SQL Server/Chapter 05/Listing 5.004.sql
@@ -1,19 +1,10 @@
-CREATE TABLE Inventory (
-  Color varchar(15) NULL,
-  Size  varchar(5) NULL,
-  Quantity int NULL
-);
+-- Ensure you've run Item30StructureAndData.sql in the Sample Databases folder
+-- in order to create the Inventory table
 
-INSERT INTO Inventory (Color, Size, Quantity) VALUES ('Red', 'L', 10);
-INSERT INTO Inventory (Color, Size, Quantity) VALUES ('Blue', 'M', 20);
-INSERT INTO Inventory (Color, Size, Quantity) VALUES ('Red', 'M', 15);
-INSERT INTO Inventory (Color, Size, Quantity) VALUES ('Blue', 'L', 5);
-
+USE Item30Example;
+GO
 
 --Listing 5.4 ROLLUP sample query
 SELECT Color, Size, SUM(Quantity)
 FROM Inventory 
 GROUP BY ROLLUP (Color, Size);
-
-
-DROP TABLE Inventory;
