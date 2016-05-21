93bc90c3d170173427dd82a6c8bcd69da492634e
DJSteele <djsteele@gmail.com>
2016-05-21 13:08:14 -0400

    Revert "Sample File"
    This reverts commit 18e0ff48e00942469648a0dacccad7e3c82f60d1.

--- Microsoft SQL Server/Chapter 02/Listing 2.016.sql
+++ Microsoft SQL Server/Chapter 02/Listing 2.016.sql
@@ -8,5 +8,9 @@ GO
 -- Listing 2.16 Sample query SQL with WHERE clause
 SELECT CustomerID, Sum(OrderTotal)
 FROM Orders
+<<<<<<< HEAD
+WHERE OrderDate > '2016-04-01'
+=======
 WHERE OrderDate > '2015-12-01'
+>>>>>>> origin/master
 GROUP BY CustomerID;
