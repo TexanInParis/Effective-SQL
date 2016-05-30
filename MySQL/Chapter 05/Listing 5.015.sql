c614e373d7575dee9df099f3e7d6a40a2ace73bf
TexanInParis <john@viescas.com>
2016-05-30 23:46:13 +0200

    Chapter 5 examples

--- MySQL/Chapter 05/Listing 5.015.sql
+++ MySQL/Chapter 05/Listing 5.015.sql
@@ -2,10 +2,28 @@
 -- and SalesOrdersData.sql in the Sample Databases folder
 -- in order to run this example. 
 
--- NOTE: MySQL refuses to run this query, saying it can't find P.CategoryID referenced in
---       the WHERE clause.  This query runs just fine in other database systems.
+-- NOTE: MySQL cannot run the original query, saying it can't find P.CategoryID referenced in
+--       the WHERE clause.  MySQL also doesn't support Common Table Expressions (solution used
+--       for DB2).  Using temporary Views instead.
 
 USE SalesOrdersSample;
+
+CREATE VIEW TotalPerProduct AS
+SELECT P2.CategoryID, 
+       SUM(OD2.QuotedPrice * OD2.QuantityOrdered) 
+       AS SumCategory 
+      FROM Products AS P2 
+      INNER JOIN Order_Details AS OD2 
+        ON P2.ProductNumber = OD2.ProductNumber 
+      INNER JOIN Orders AS O2
+        ON O2.OrderNumber = OD2.OrderNumber
+      WHERE O2.OrderDate BETWEEN '2015-10-01' AND '2015-12-31'
+      GROUP BY P2.CategoryID, P2.ProductNumber;
+	  
+CREATE VIEW AveragePerCategory AS
+SELECT t.CategoryID, AVG(t.SumCategory) AS AverageOfCategory 
+   FROM TotalPerProduct AS t
+   GROUP BY t.CategoryID;
 
 SELECT P.CategoryID, C.CategoryDescription, P.ProductName, 
   SUM(OD.QuotedPrice * OD.QuantityOrdered) AS TotalSales
@@ -19,17 +37,9 @@ FROM Products AS P
 WHERE O.OrderDate BETWEEN '2015-10-01' AND '2015-12-31'
 GROUP BY P.CategoryID, C.CategoryDescription, P.ProductName
 HAVING SUM(OD.QuotedPrice * OD.QuantityOrdered) > 
-  (SELECT AVG(SumCategory) 
-   FROM 
-     (SELECT P2.CategoryID, 
-       SUM(OD2.QuotedPrice * OD2.QuantityOrdered) 
-       AS SumCategory 
-      FROM Products AS P2 
-      INNER JOIN Order_Details AS OD2 
-        ON P2.ProductNumber = OD2.ProductNumber 
-      INNER JOIN Orders AS O2
-        ON O2.OrderNumber = OD2.OrderNumber
-      WHERE P2.CategoryID = P.CategoryID
-      AND O2.OrderDate BETWEEN '2015-10-01' AND '2015-12-31'
-      GROUP BY P2.CategoryID, P2.ProductNumber) AS S 
-GROUP BY CategoryID);
+	(SELECT a.AverageOfCategory 
+	FROM AveragePerCategory AS a 
+	WHERE a.CategoryID = P.CategoryID);
+	
+DROP VIEW AveragePerCategory;
+DROP VIEW TotalPerProduct;
