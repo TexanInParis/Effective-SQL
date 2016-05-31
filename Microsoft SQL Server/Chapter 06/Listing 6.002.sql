aff52143f9f687281aefee47de8e45e228241608
TexanInParis <john@viescas.com>
2016-05-31 12:32:34 +0200

    Chapter 6 examples

--- Microsoft SQL Server/Chapter 06/Listing 6.002.sql
+++ Microsoft SQL Server/Chapter 06/Listing 6.002.sql
@@ -28,3 +28,29 @@ AND EXISTS
         ON Products.ProductNumber = Order_Details.ProductNumber 
    WHERE Products.ProductName = 'Helmet'
       AND O3.OrderNumber = Orders.OrderNumber);
+
+-- Second example using LIKE to fetch real data
+SELECT Customers.CustomerID, Customers.CustFirstName, 
+  Customers.CustLastName, Orders.OrderNumber, Orders.OrderDate
+FROM Customers
+  INNER JOIN Orders
+    ON Customers.CustomerID = Orders.CustomerID
+WHERE EXISTS 
+  (SELECT * 
+   FROM (Orders AS O2
+      INNER JOIN Order_Details
+        ON O2.OrderNumber = Order_Details.OrderNumber)
+      INNER JOIN Products
+        ON Products.ProductNumber = Order_Details.ProductNumber 
+   WHERE Products.ProductName LIKE '%Skateboard%' 
+    AND O2.OrderNumber = Orders.OrderNumber)
+AND EXISTS 
+  (SELECT * 
+   FROM (Orders AS O3 
+      INNER JOIN Order_Details
+        ON O3.OrderNumber = Order_Details.OrderNumber)
+      INNER JOIN Products
+        ON Products.ProductNumber = Order_Details.ProductNumber 
+   WHERE Products.ProductName LIKE '%Helmet%'
+      AND O3.OrderNumber = Orders.OrderNumber);
+
