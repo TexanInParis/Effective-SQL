b3c715e724ce38cc69646887ece036793a5a8920
TexanInParis <john@viescas.com>
2016-05-31 11:38:37 +0200

    Chapter 5 example

--- IBM DB2/Chapter 05/Listing 5.016.sql
+++ IBM DB2/Chapter 05/Listing 5.016.sql
@@ -4,7 +4,23 @@
 
 SET SCHEMA SalesOrdersSample;
 
-WITH CatProdData AS (
+-- Cannot use nested subqueries with direct references so CTEs are used instead.
+WITH TotalPerProductNumber AS (
+  SELECT 
+  	P2.CategoryID, P2.ProductNumber,
+   SUM(OD2.QuotedPrice * OD2.QuantityOrdered) AS SumCategory 
+  FROM Products AS P2 
+  INNER JOIN Order_Details AS OD2 
+    ON P2.ProductNumber = OD2.ProductNumber 
+  INNER JOIN Orders AS O2
+    ON O2.OrderNumber = OD2.OrderNumber
+  WHERE O2.OrderDate BETWEEN DATE '2015-10-01' AND DATE '2015-12-31'
+  GROUP BY P2.CategoryID, P2.ProductNumber
+), AveragePerCategory AS (
+   SELECT t.CategoryID, AVG(t.SumCategory) AS AverageOfCategory 
+   FROM TotalPerProductNumber AS t
+   GROUP BY t.CategoryID
+), CatProdData AS (
 	SELECT C.CategoryID, C.CategoryDescription, P.ProductName, OD.QuotedPrice, OD.QuantityOrdered
 	FROM Products AS P 
 	   INNER JOIN Order_Details AS OD 
@@ -20,12 +36,8 @@ SELECT D.CategoryDescription, D.ProductName,
 FROM CatProdData AS D
 GROUP BY D.CategoryID, D.CategoryDescription, D.ProductName
 HAVING SUM(D.QuotedPrice * D.QuantityOrdered) > 
-  (SELECT AVG(SumCategory) 
-   FROM 
-    (SELECT D2.CategoryID, 
-      SUM(D2.QuotedPrice * D2.QuantityOrdered) 
-        AS SumCategory 
-     FROM CatProdData AS D2
-     GROUP BY D2.CategoryID, D2.ProductName) AS S
-    WHERE S.CategoryID = D.CategoryID
-	GROUP BY S.CategoryID);
+  (
+	SELECT a.AverageOfCategory 
+	FROM AveragePerCategory AS a 
+	WHERE a.CategoryID = D.CategoryID
+);
