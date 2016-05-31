1ec83a3a7d60664d34698f404d9e97003207524c
bclothier <bgclothier@gmail.com>
2016-05-31 08:31:24 -0500

    Fix up 2.29 and 2.30 for DB2 and Oracle

--- IBM DB2/Chapter 02/Listing 2.030.sql
+++ IBM DB2/Chapter 02/Listing 2.030.sql
@@ -1,16 +1,15 @@
--- Ensure you've run SalesOrdersStructure.sql and SalesOrdersData.sql 
--- in the Sample Databases folder in order to run this example. 
+-- Ensure you've run SalesOrdersStructure.sql
+-- and SalesOrdersData.sql in the Sample Databases folder
+-- in order to run this example. 
 
 SET SCHEMA SalesOrdersSample;
 
-ALTER TABLE Employees
-ADD COLUMN EmpDOB date;
+-- Ensure you've run Listing 2.029.sql to create the CalculateAge function
 
-CREATE FUNCTION CalculateAge(Date_of_Birth DATE) 
-RETURNS int
-RETURN TRUNC((CURRENT_DATE - Date_of_Birth) / 365);
+-- Listing 2.30 SQL statement using CalculateAge function
+SELECT EmployeeID, EmpFirstName, EmpLastName, SalesOrdersSample.CalculateAge(EmpDOB) AS EmpAge
+  FROM Employees
+WHERE SalesOrdersSample.CalculateAge(EmpDOB) > 50;
 
-DROP FUNCTION CalculateAge;
-
-ALTER TABLE Employees
-DROP COLUMN EmpDOB;
+-- Run the following if you do not wish to keep the function in the database.
+-- DROP FUNCTION CalculateAge;
--- Oracle/Chapter 02/Listing 2.029.sql
+++ Oracle/Chapter 02/Listing 2.029.sql
@@ -1,3 +1,6 @@
+-- Ensure you've run SalesOrdersStructure.sql and SalesOrdersData.sql 
+-- in the Sample Databases folder in order to run this example. 
+
 ALTER SESSION SET CURRENT_SCHEMA = SalesOrdersSample;
 
 CREATE FUNCTION CalculateAge(Date_of_Birth IN DATE) 
