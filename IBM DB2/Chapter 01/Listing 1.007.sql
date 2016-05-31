4e35ac57d278d152d36f9191a93c15c4518c788d
bclothier <bgclothier@gmail.com>
2016-05-31 04:33:13 -0500

    Verify & fix 1.007

--- IBM DB2/Chapter 01/Listing 1.007.sql
+++ IBM DB2/Chapter 01/Listing 1.007.sql
@@ -6,10 +6,15 @@ SET SCHEMA SalesOrdersSample;
 
 -- Turn off integrity so we can change the table
 SET INTEGRITY FOR Order_Details OFF;
+
 -- Create the calculated column using an expression
 ALTER TABLE Order_Details
   ADD COLUMN ExtendedPrice decimal (15, 2)
   GENERATED ALWAYS AS (QuantityOrdered * QuotedPrice);
+
+-- Turn integrity back on
+SET INTEGRITY FOR Order_Details IMMEDIATE CHECKED FORCE GENERATED;
+
 -- Index the calculated column
 CREATE INDEX Order_Details_ExtendedPrice
 ON Order_Details (ExtendedPrice);
@@ -17,5 +22,4 @@ ON Order_Details (ExtendedPrice);
 -- Now clean it up
 ALTER TABLE Order_Details DROP COLUMN ExtendedPrice;
 
--- Turn integrity back on
-SET INTEGRITY FOR Order_Details IMMEDIATE CHECKED FORCE GENERATED;
+CALL SYSPROC.ADMIN_CMD('REORG TABLE SALESORDERSSAMPLE.ORDER_DETAILS');
