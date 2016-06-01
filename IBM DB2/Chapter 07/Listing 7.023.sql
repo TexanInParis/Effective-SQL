09f0c944029d721817fff4acc6f07f06f61e11a1
bclothier <bgclothier@gmail.com>
2016-06-01 04:40:35 -0500

    Add missing listings for DB2's Chapter 7

---
+++ IBM DB2/Chapter 07/Listing 7.023.sql
@@ -0,0 +1,13 @@
+-- Ensure you've run SalesOrdersStructure.sql
+-- and SalesOrdersData.sql in the Sample Databases folder
+-- in order to run this example. 
+
+SET SCHEMA SalesOrdersSample;
+
+SELECT p.*
+FROM Products AS p
+WHERE NOT EXISTS (
+    SELECT NULL
+    FROM Order_Details AS d
+    WHERE p.ProductNumber = d.ProductNumber
+);
---
+++ IBM DB2/Chapter 07/Listing 7.025.sql
@@ -0,0 +1,18 @@
+-- Ensure you've run SalesOrdersStructure.sql
+-- and SalesOrdersData.sql in the Sample Databases folder
+-- in order to run this example. 
+
+SET SCHEMA SalesOrdersSample;
+
+CREATE INDEX IX_Orders_EmployeeID_Included
+ON Orders (EmployeeID, OrderNumber, CustomerID);
+
+SELECT o.OrderNumber, o.CustomerID
+FROM Orders AS o
+WHERE EmployeeID = 751;
+
+SELECT o.OrderNumber, o.CustomerID
+FROM Orders AS o
+WHERE EmployeeID = 708;
+
+DROP INDEX IX_Orders_EmployeeID_Included;
