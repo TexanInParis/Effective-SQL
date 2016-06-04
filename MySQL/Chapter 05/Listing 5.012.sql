934bcb776766a8e66b0e2e7d2ba7499a6b286e82
bclothier <bgclothier@gmail.com>
2016-06-03 20:57:17 -0500

    MySQL Chapter 5 Listings

---
+++ MySQL/Chapter 05/Listing 5.012.sql
@@ -0,0 +1,15 @@
+-- Ensure you've run SalesOrdersStructure.sql
+-- and SalesOrdersData.sql in the Sample Databases folder
+-- in order to run this example. 
+
+USE SalesOrdersSample;
+
+SELECT c.CustomerID, c.CustFirstName, c.CustLastName, c.CustState,
+  o.LastOrderDate, o.OrderCount, o.TotalAmount
+FROM Customers AS c
+LEFT JOIN 
+   (SELECT t.CustomerID, MAX(t.OrderDate) AS LastOrderDate,
+    COUNT(t.OrderNumber) AS OrderCount, SUM(t.OrderTotal) AS TotalAmount
+    FROM Orders AS t
+    GROUP BY t.CustomerID) AS o
+  ON c.CustomerID = o.CustomerID;
