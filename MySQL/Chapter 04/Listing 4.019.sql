39f714262dd3907bccc9e8c82dcdfe3f625d16e3
TexanInParis <john@viescas.com>
2016-08-29 18:46:21 +0200

    Sample files

--- MySQL/Chapter 04/Listing 4.019.sql
+++ MySQL/Chapter 04/Listing 4.019.sql
@@ -34,8 +34,14 @@ DROP VIEW ProdsOfInterest;
 
 -- Sample query that searches products correctly:
 CREATE VIEW CustomerProducts AS
-SELECT DISTINCT Customers.CustomerID, Customers.CustFirstName, 
-  Customers.CustLastName, Products.ProductName
+SELECT DISTINCT Customers.CustomerID, Customers.CustFirstName,
+  Customers.CustLastName,
+       CASE WHEN Products.ProductName LIKE '%Skateboard%' THEN 'Skateboard'
+              WHEN Products.ProductName LIKE '%Helmet%' THEN 'Helmet'
+              WHEN Products.ProductName LIKE '%Knee Pads%' THEN 'Knee Pads'
+              WHEN Products.ProductName LIKE '%Gloves%' THEN 'Gloves'
+              ELSE NULL
+       END AS ProductCategory
 FROM Customers INNER JOIN Orders
   ON Customers.CustomerID = Orders.CustomerID
 INNER JOIN Order_Details
@@ -44,21 +50,27 @@ INNER JOIN Products
   ON Products.ProductNumber = Order_Details.ProductNumber;
 
 CREATE VIEW ProdsOfInterest AS
-SELECT Products.ProductName
+SELECT Products.ProductName,
+       CASE WHEN Products.ProductName LIKE '%Skateboard%' THEN 'Skateboard'
+              WHEN Products.ProductName LIKE '%Helmet%' THEN 'Helmet'
+              WHEN Products.ProductName LIKE '%Knee Pads%' THEN 'Knee Pads'
+              WHEN Products.ProductName LIKE '%Gloves%' THEN 'Gloves'
+              ELSE NULL
+       END AS ProductCategory
 FROM Products
 WHERE ProductName LIKE '%Skateboard%'
-   OR ProductName LIKE '%Helmet%' 
-   OR ProductName LIKE '%Knee Pads%' 
+   OR ProductName LIKE '%Helmet%'
+   OR ProductName LIKE '%Knee Pads%'
    OR ProductName LIKE '%Gloves%';
 
 SELECT DISTINCT CustomerID, CustFirstName, CustLastName
 FROM CustomerProducts AS CP1
 WHERE NOT EXISTS
-  (SELECT ProductName FROM ProdsOfInterest AS PofI
+  (SELECT ProductCategory FROM ProdsOfInterest AS PofI
     WHERE NOT EXISTS
     (SELECT CustomerID FROM CustomerProducts AS CP2
       WHERE CP2.CustomerID = CP1.CustomerID
-      AND CP2.ProductName = PofI.ProductName));
+      AND CP2.ProductCategory = PofI.ProductCategory));
 
 DROP VIEW CustomerProducts;
 DROP VIEW ProdsOfInterest;
